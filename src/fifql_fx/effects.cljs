5bf4077648c3411155380892bb9e89122cb8e670
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-02-11 01:06:18 -0500

    Effect added

--- src/fifql_fx/effects.cljs
+++ src/fifql_fx/effects.cljs
@@ -1,3 +1,16 @@
 (ns fifql-fx.effects
   (:require
-   [reframe.core :as reframe]))
+   [reframe.core :as reframe :refer [reg-fx dispatch]]
+   [fifql.client :refer [query]]
+   [fif.stack-machine.error-handling :refer [error-object?]]))
+
+
+(reg-fx
+  :fifql/query
+  (fn [{:keys [url form on-success on-failure]}]
+    (query url form
+           (fn [result]
+             (if (and on-failure (-> result :stack first error-object?))
+               (dispatch [on-failure result])
+               (dispatch [on-success result]))))))
+    
